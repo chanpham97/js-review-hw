d2fefecdeadbfe14495d51adfddcced806cd1b71
Chan Pham <iamchanpham@gmail.com>
2025-01-15 18:12:43 +0000

    hw template

---
+++ script.js
@@ -0,0 +1,26 @@
+// Task 1: Create variable called 'storeName' and set it to "CSH Store". Console log "Welcome to [storeName]"
+
+
+// Task 2: Create a user object with properties 'username' (any), 'password' (any), and 'status' (online or offline). 
+
+
+// Task 3: Console log "[username] is [status]".
+
+
+// Task 4: Prompt the user to enter their password and save it in a variable called passwordGuess
+
+
+// Task 5: If passwordGuess is equal to the password, console log "Welcome [username]" and change status to "online". Else, console log "Wrong password" and change status to "offline"
+
+
+// Task 6: Create an object called hoodieInventory with the properties 'name' ("hoodie"), 'quantity' (any number), and 'price' (any number). 
+
+
+// Task 7: Console log "[name] costs [price]"
+
+
+// Task 8: You just sold a hoodie! Change the quantity to be smaller. Console log "[quantity] [name] left in inventory." Confirm that the number changed!
+
+
+// Extra credit: Change task 5 such that if the user guesses the password wrong, it keeps prompting them until they guess correctly!
+
